ff31bb2c85125f87fab5252219483f68ff25b866
Jan Noha <nohajc@gmail.com>
2023-12-27 21:57:53 +0100

    minimal example to reproduce a bug

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	_ "embed"
+
+	"fyne.io/systray"
+)
+
+//go:embed st-icon.png
+var appIcon []byte
+
+func main() {
+	systray.Run(onReady, func() {})
+}
+
+type ItemGroup struct {
+	checkedIdx int
+	items      []*systray.MenuItem
+}
+
+func (g *ItemGroup) AddItems(items ...*systray.MenuItem) {
+	if len(items) == 0 {
+		return
+	}
+	if len(g.items) == 0 {
+		items[0].Check()
+	}
+	g.items = append(g.items, items...)
+}
+
+func (g *ItemGroup) Check(idx int) {
+	if idx < 0 || idx >= len(g.items) {
+		return
+	}
+	g.items[g.checkedIdx].Uncheck()
+	g.items[idx].Check()
+	g.checkedIdx = idx
+}
+
+func onReady() {
+	systray.SetTemplateIcon(appIcon, appIcon)
+	item1 := systray.AddMenuItemCheckbox("item 1", "", true)
+	item2 := systray.AddMenuItemCheckbox("item 2", "", false)
+	group1 := &ItemGroup{}
+	group1.AddItems(item1, item2)
+
+	item3 := systray.AddMenuItem("submenu", "")
+	subitem1 := item3.AddSubMenuItemCheckbox("subitem 1", "", true)
+	subitem2 := item3.AddSubMenuItemCheckbox("subitem 2", "", false)
+	group2 := &ItemGroup{}
+	group2.AddItems(subitem1, subitem2)
+
+	for {
+		select {
+		case <-item1.ClickedCh:
+			group1.Check(0)
+		case <-item2.ClickedCh:
+			group1.Check(1)
+		case <-subitem1.ClickedCh:
+			group2.Check(0)
+		case <-subitem2.ClickedCh:
+			group2.Check(1)
+		}
+	}
+}
